d6303d20522e8fea051f78fca3ce3a3c849a15f6
covidnetworks <71368604+covidnetworks@users.noreply.github.com>
2021-01-20 11:42:02 -0800

    testimonial slider

---
+++ js/function.js
@@ -0,0 +1,17 @@
+/* Code By Webdevtrick ( https://webdevtrick.com ) */
+$(document).ready(function () {
+    $('.testiSlide').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        responsive: [{
+        breakpoint: 850,
+        settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        }
+        }]
+    });
+});
