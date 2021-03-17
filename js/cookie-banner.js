8d3e797ab8945f87f9e7c264093398a358036bc7
tweoss <hwymonkey@gmail.com>
2021-03-16 21:06:31 -0700

    Cookie Banner cookies

--- js/cookie-banner.js
+++ js/cookie-banner.js
@@ -1,7 +1,16 @@
-if (localStorage.getItem('cookieSeen') != 'shown') {
-  $('.cookie-banner').delay(2000).fadeIn();
-  localStorage.setItem('cookieSeen','shown')
-}
-$('.close').click(function() {
-  $('.cookie-banner').fadeOut();
-})
+$('.close-cookie-banner').click(function() {
+    var now = new Date();
+    var time = now.getTime();
+    var expireTime = time + 2 * 365 * 24 * 60 * 60; // two years
+    now.setTime(expireTime);
+    document.cookie = 'cookieAccepted=true; expires=' + now.toUTCString() + '; path=/; ';
+    $('.cookie-banner').slideUp("slow");
+});
+
+$(document).ready(function() {
+    if (!(document.cookie.includes('cookieAccepted=true'))) {
+        for (let item of document.getElementsByClassName('cookie-banner')) {
+            item.style.display = 'unset';
+        }
+    }
+});
